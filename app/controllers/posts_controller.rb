1ce844acf217ba6884a0539e1b198a6c10ae463e
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-11 10:57:10 +0200

    Add actions to Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,34 @@
 class PostsController < ApplicationController
-  def index
-  end
+  
+def index
+    @posts = Post.all
+end
+
+
+def new
+    @post= Post.new
+end
+
+def create
+    @post = current_user.posts.build(use_params)
+
+    if @post.save
+        redirect_to root_path
+
+    else
+        render :new
+    end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+
+  redirect_to root_path
+end
+
+private
+    def use_params
+        params.require(:post).permit(:title, :body)
+    end
 end
